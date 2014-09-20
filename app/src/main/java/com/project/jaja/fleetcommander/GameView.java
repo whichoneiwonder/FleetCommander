71ee8081ec9e5b1ad832b73f7fb099df0f24054e
Avnish Jain <avnishj@student.unimelb.edu.au>
2014-09-20 19:17:51 +1000

    Removes ship when tapped on and added edge collision. Speed is random for now

--- app/src/main/java/com/project/jaja/fleetcommander/GameView.java
+++ app/src/main/java/com/project/jaja/fleetcommander/GameView.java
@@ -5,8 +5,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,18 +19,24 @@ import android.view.SurfaceView;
 public class GameView extends SurfaceView {
 
     private Bitmap map;
+
     private SurfaceHolder surfaceHolder;
     private GameLoopThread thread;
-    private ShipSprite ship;
+    private ArrayList<ShipSprite> ships;
+    private int numShips;
+    private long timeTilllastClick;
 
     public GameView(Context context){
         super(context);
         thread = new GameLoopThread(this);
+        ships = new ArrayList<ShipSprite>();
+        numShips = 0;
         surfaceHolder = getHolder();
         surfaceHolder.addCallback(new SurfaceHolder.Callback() {
 
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
+                populateShips();
                 thread.setGameState(true);
                 thread.start();
             }
@@ -53,14 +63,71 @@ public class GameView extends SurfaceView {
             }
         });
 
-        map = BitmapFactory.decodeResource(getResources(), R.drawable.player_ship);
-        ship = new ShipSprite(this, map, 5, 10);
+
+    }
+
+    protected void populateShips(){
+        ships.add(newShip(R.drawable.player_ship));
+        ships.add(newShip(R.drawable.player_ship));
+        ships.add(newShip(R.drawable.player_ship));
+    }
+
+    protected ShipSprite newShip(int drawable){
+        map = BitmapFactory.decodeResource(getResources(), drawable);
+
+        ShipSprite newShip = new ShipSprite(this, map, (numShips * 110)+30, 10);
+        numShips++;
+        return newShip;
     }
 
     protected void onDraw(Canvas canvas){
         canvas.drawColor(Color.rgb(0,153,204));
-        ship.onDraw(canvas);
+
+        for(ShipSprite ship : ships){
+            ship.onDraw(canvas);
+        }
 
     }
+
+    public boolean isShipColliding(){
+        for(int i = 0; i < ships.size(); i++){
+            for(int j = 0; j < ships.size(); j++){
+                if (i != j && checkCollision(ships.get(i), ships.get(j))){
+                    Log.i("Collision", "Collision between ships");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkCollision(ShipSprite shipOne, ShipSprite shipTwo){
+        if(shipOne.isColliding(shipTwo.getXPosition(),shipTwo.getYPosition())
+                || shipOne.isColliding(shipTwo.getXPosition() + map.getWidth(), shipTwo.getYPosition())
+                || shipOne.isColliding(shipTwo.getXPosition(), shipTwo.getYPosition() + map.getHeight())
+                || shipOne.isColliding(shipTwo.getXPosition() + map.getWidth(), shipTwo.getYPosition() + map.getWidth())
+                ){
+                    return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (System.currentTimeMillis() - timeTilllastClick > 500) {
+            timeTilllastClick = System.currentTimeMillis();
+            synchronized (getHolder()) {
+               for(ShipSprite ship: ships){
+                    if (ship.isColliding(event.getX(), event.getY())) {
+                        //Do something with the ships here
+                        ships.remove(ship);
+                        break;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
 
 }
--- app/src/main/java/com/project/jaja/fleetcommander/ShipSprite.java
+++ app/src/main/java/com/project/jaja/fleetcommander/ShipSprite.java
@@ -2,7 +2,6 @@ package com.project.jaja.fleetcommander;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.util.Log;
 
 /**
  * Created by avnishjain on 19/09/14.
@@ -12,6 +11,7 @@ public class ShipSprite {
     private GameView gameView;
     private Bitmap map;
     private int xSpeed;
+    private int ySpeed;
     private int xPosition;
     private int yPosition;
     public final static int pixelOffset = 5;
@@ -19,27 +19,58 @@ public class ShipSprite {
     public ShipSprite(GameView gameView, Bitmap map, int xPosition, int yPosition){
         this.gameView = gameView;
         this.map = map;
+        //this.xPosition = (int)(Math.random() * (gameView.getWidth() - map.getWidth())) + 1;
+        //this.xPosition = (int)(Math.random() * (gameView.getHeight() - map.getHeight())) + 1;
         this.xPosition = xPosition;
         this.yPosition = yPosition;
+        this.xSpeed = (int) (Math.random()*10)+1;
+        this.ySpeed = (int) (Math.random()*10)+1;
+        //this.xSpeed = 1;
+        //this.ySpeed = 1;
+
+
     }
 
     private void update(){
-        Log.i("Debugging", "Ship Position : (" + xPosition + ", " + yPosition + ")\n" );
-        Log.i("Debugging", "Edge of Screen : " + (gameView.getWidth() - map.getWidth()) + "\n\n");
+        //Log.i("Debugging", "Ship Position : (" + xPosition + ", " + yPosition + ")\n" );
+        //Log.i("Debugging", "Edge of Screen : " + (gameView.getWidth() - map.getWidth()) + "\n\n");
 
-        if (xPosition == gameView.getWidth() - map.getWidth() - pixelOffset) {
-            xSpeed = -1 ;
+        if (xPosition > gameView.getWidth() - map.getWidth() - xSpeed || xPosition + xSpeed < 0) {
+            xSpeed = -xSpeed ;
         }
-        if (xPosition == pixelOffset) {
-            xSpeed = 1;
+
+        if (yPosition > gameView.getHeight() - map.getHeight() - ySpeed || yPosition + ySpeed < 0) {
+            ySpeed = -ySpeed ;
         }
 
         xPosition = xPosition + xSpeed;
+        yPosition = yPosition + ySpeed;
+
     }
 
     public void onDraw(Canvas canvas) {
         update();
         canvas.drawBitmap(map, xPosition , yPosition, null);
+    }
+
+    public boolean isColliding (float x, float y) {
+        return x > xPosition && x < xPosition + map.getWidth() && y > yPosition && y < yPosition + map.getHeight();
+    }
+
+    public int getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(int xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public int getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(int ySpeed) {
+        this.ySpeed = ySpeed;
     }
 
     public int getXPosition() {
@@ -56,13 +87,5 @@ public class ShipSprite {
 
     public void setYPosition(int yPosition) {
         this.yPosition = yPosition;
-    }
-
-    public int getXSpeed() {
-        return xSpeed;
-    }
-
-    public void setXSpeed(int xSpeed) {
-        this.xSpeed = xSpeed;
     }
 }
